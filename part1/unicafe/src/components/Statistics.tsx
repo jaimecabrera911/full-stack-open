0a4c2d3ce7e73b476e59786cd95c0c837d5a3670
jaimeacabreraa <JCABR16@bancodebogota.com.co>
2025-04-15 23:49:42 -0500

    Ejercicio 1.7 1.8

--- part1/unicafe/src/components/Statistics.tsx
+++ part1/unicafe/src/components/Statistics.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
 const Statistics = (props) => {
-  return (
-    <div>
-        <h1>statistics</h1>
-        <p>good {props.good}</p>
-        <p>neutral {props.neutral}</p>
-        <p>bad {props.bad}</p>
-    </div>
-  )
+
+    const total = props.good + props.neutral + props.bad
+    const avergare = (props.good - props.bad) / (props.good + props.neutral + props.bad)
+    const positive = props.good / (props.good + props.neutral + props.bad)
+    return (
+        <div>
+            <h1>statistics</h1>
+            <p>good {props.good}</p>
+            <p>neutral {props.neutral}</p>
+            <p>bad {props.bad}</p>
+            <p>all {total}</p>
+            <p>average {isNaN(avergare) ? 0 : avergare}</p>
+            <p>positive {isNaN(positive) ? 0 : positive *100 + '%'}</p>
+        </div>
+    )
 }
 
 export default Statistics
